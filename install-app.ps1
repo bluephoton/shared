5c1e25d0a266d94f9ec38b4f8dbe5871be969c7f
Mohamed <bluephoton@users.noreply.github.com>
2020-09-01 01:29:22 -0700

    Update install-app.ps1

--- install-app.ps1
+++ install-app.ps1
@@ -9,20 +9,20 @@ $nodeMsi = "$($appFolder)\node.msi"
 
 
 function DownloadFile([string] $fileUrl, [string] $destination) {
-    Write-Host "Downloading '$fileUrl' to '$destination' ..."
+    "Downloading '$fileUrl' to '$destination' ..."
     try {
         $webClient = New-Object System.Net.WebClient
         $webClient.DownloadFile($fileUrl, $destination)
-        Write-Host "Downloading complete"
+        "Downloading complete"
     } catch {
-        Write-Host "Download failed: $($_.Exception)"
+        "Download failed: $($_.Exception)"
         throw $_
     }
 }
 
 # create app folder if it doesn't exist
 if(!(Test-Path -PathType Container -Path $appFolder)) {
-    Write-Host "App folder not found, creating ..."
+    "App folder not found, creating ..."
     New-Item -ItemType Directory -Path $appFolder | Out-Null
 }
 
@@ -30,26 +30,26 @@ if(!(Test-Path -PathType Container -Path $appFolder)) {
 $currentNodeVersion = ""
 if (Get-Command node -errorAction SilentlyContinue) {
     $currentNodeVersion = (node -v)
-    Write-Host "Node installed. Version: $($currentNodeVersion)"
+    "Node installed. Version: $($currentNodeVersion)"
 }
 
 if($nodeVersion -eq $currentNodeVersion) {
-    Write-Host "Node already installed"
+    "Node already installed"
 } else {
     # Download node
     DownloadFile -fileUrl $nodeDownloadUrl -destination $nodeMsi
 
     # Install node
-    write-host "Installing node ..."
+    "Installing node ..."
     $args = "/i", $nodeMsi, "/quiet", "/norestart"
     Start-Process "msiexec" -ArgumentList $args -Wait
-    write-host "Installing node complete"
+    "Installing node complete"
 }
 
 # install app files
-write-host "Installing app files ..."
+"Installing app files ..."
 $appFiles | % { DownloadFile -fileUrl "$($appFilesBareUrl)$($_)" -destination "$($appFolder)\$($_)" }
-write-host "Installing app files complete"
+"Installing app files complete"
 
 # Launch app and don't wait
 Set-Location -Path $appFolder
